d36b357d60f7de3dbcaaaa147551ea57671f4cdd
kasatejaswarreddy0512 <tejaswarreddy0512@gmail.com>
2026-03-28 11:28:00 +0530

    service offering

---
+++ service-offering/src/main/java/com/ktsr/service/offering/ServiceOfferingApplication.java
@@ -0,0 +1,13 @@
+package com.ktsr.service.offering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOfferingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOfferingApplication.class, args);
+	}
+
+}
